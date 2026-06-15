e766c49149dc5ad7c1d145874c9d81b363ac31f4
tingwei.liu.19930301 <willy11342002@gmail.com>
2026-06-15 10:19:33 +0800

    fix(nav): notif panel and user menu are mutually exclusive

--- apps/web/components/layout/AppNav.vue
+++ apps/web/components/layout/AppNav.vue
@@ -17,7 +17,7 @@
 
         <!-- 通知鈴鐺 -->
         <div class="nav__notif" ref="notifEl">
-          <button class="nav__notif-btn" @click.stop="notifOpen = !notifOpen">
+          <button class="nav__notif-btn" @click.stop="notifOpen = !notifOpen; if (notifOpen) menuOpen = false">
             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round">
               <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/>
               <path d="M13.73 21a2 2 0 0 1-3.46 0"/>
@@ -53,7 +53,7 @@
 
         <!-- 使用者頭像 + 下拉選單 -->
         <div class="nav__user">
-          <button class="nav__avatar" @click.stop="menuOpen = !menuOpen">
+          <button class="nav__avatar" @click.stop="menuOpen = !menuOpen; if (menuOpen) notifOpen = false">
             <img v-if="avatarUrl" :src="avatarUrl" :alt="displayName" referrerpolicy="no-referrer">
             <span v-else>{{ initials }}</span>
           </button>
